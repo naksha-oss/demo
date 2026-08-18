899f1a89835527d4f565384a12d14ea11538eeef
phmai <phuc.mai@here.com>
2026-08-18 13:21:03 +0200

    demo 2 adjust

--- src/main/kotlin/naksha/demo/demo_02_query_history.kt
+++ src/main/kotlin/naksha/demo/demo_02_query_history.kt
@@ -2,6 +2,7 @@ package naksha.demo
 
 import naksha.base.Guid
 import naksha.base.Int64
+import naksha.base.Platform
 import naksha.base.Version
 import naksha.geo.SpPoint
 import naksha.model.objects.NakshaFeature
@@ -97,6 +98,7 @@ fun main(vararg args: String) {
     for (feature in random_features) {
         feature.geometry = point
     }
+    println("Adding 5 features with geometry being a point (1,1) to the random data collection...")
     val features_added = demo.writeFeatures(RANDOM_DATA_COLLECTION_ID, *random_features)
     val ADDED_VERSION = (Guid.fromString(features_added[0].properties.xyz.uuid!!).tupleNumber.version.toLong() and -4) or 3
 
@@ -107,9 +109,12 @@ fun main(vararg args: String) {
         feature.properties["demoUpdate"] = "to mark update"
         feature.geometry = newPoint
     }
+    println("Updating first 3 features to new geometry point (2,2)...")
+    println("Input to UPDATE call must have correct UUID as returned from the previous write, to ensure atomicity.")
     val updatedFeatures = demo.updateFeatures(RANDOM_DATA_COLLECTION_ID, *update_features)
     val UPDATED_VERSION = (Guid.fromString(updatedFeatures[0].properties.xyz.uuid!!).tupleNumber.version.toLong() and -4) or 3
     //step 4
+    println("Deleting the first feature '${update_features[0].id}' that was updated, to mark it as deleted in HEAD...")
     val deletedFeatures = demo.deleteFeatures(RANDOM_DATA_COLLECTION_ID, update_features[0])
     val DELETED_VERSION = (Guid.fromString(deletedFeatures[0].properties.xyz.uuid!!).tupleNumber.version.toLong() and -4) or 3
     //step 5??
@@ -117,10 +122,14 @@ fun main(vararg args: String) {
     val readResponseOriginal =
         demo.readFeaturesByIds(RANDOM_DATA_COLLECTION_ID, INITIAL_VERSION.number, random_features[0].id, random_features[1].id)
     println("Reading features for version before insert, found number of features: ${readResponseOriginal.features.size}")
+    println("Features JSON content, demonstrating JSON parser, can be Jackson or Naksha internal parser (historically from Wikvaya project): ")
+    for (feature in readResponseOriginal) println(Platform.toJSON(feature))
     //step 7
     val readResponseAfterInsert =
         demo.readFeaturesByIds(RANDOM_DATA_COLLECTION_ID, Int64(ADDED_VERSION), random_features[0].id, random_features[1].id)
-    println("Reading first 2 features after insert, even though 1st one is deleted in HEAD: ${readResponseAfterInsert.features}")
+    println("Reading first 2 features at the specific version after insert, so even though 1st one is deleted in HEAD, its history is preserved. Found number of features: ${readResponseAfterInsert.features.size}")
+    println("Features content: ")
+    for (feature in demo.successResponse(readResponseAfterInsert)) println(feature)
     //step 8
     val readResponseAfterUpdate = demo.readFeaturesByIds(
         RANDOM_DATA_COLLECTION_ID,
@@ -129,7 +138,9 @@ fun main(vararg args: String) {
         random_features[1].id,
         random_features[2].id
     )
-    println("Reading the 3 updated features, including the 1st one that is deleted in HEAD: ${readResponseAfterUpdate.features}")
+    println("Reading the 3 updated features, including the 1st one that is deleted in HEAD. Found number of features: ${readResponseAfterUpdate.features.size}")
+    println("Features content: ")
+    for (feature in demo.successResponse(readResponseAfterUpdate)) println(feature)
 
     println("\n\nArguments for demo_03:\n$INITIAL_VERSION $ADDED_VERSION $UPDATED_VERSION $DELETED_VERSION\n")
 }
